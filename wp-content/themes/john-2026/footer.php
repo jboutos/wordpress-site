55158803c7ac028ace796fe7905b9d7413ca8d43
jboutos <215729236+jboutos@users.noreply.github.com>
2026-01-12 22:29:24 +0200

    Initial commit

---
+++ wp-content/themes/john-2026/footer.php
@@ -0,0 +1,10 @@
+		</main>
+
+		<footer>
+			<h2>Footer</h2>
+			<p class='calm-voice'>© <?php echo date("Y"); ?> and beyond</p>
+		</footer>
+
+		<?php wp_footer(); ?>
+	</body>
+</html>
---
+++ wp-content/themes/john-2026/functions.php
@@ -0,0 +1,11 @@
+<?php 
+
+/* Disable WordPress Admin Bar for all users */
+add_filter( 'show_admin_bar', '__return_false' );
+
+// load css into the website's front-end
+function mytheme_enqueue_style() {
+	wp_enqueue_style( 'style', get_stylesheet_uri()); 
+}
+add_action( 'wp_enqueue_scripts', 'mytheme_enqueue_style');
+?>
